3f618323bbedf6e9f5ee992e6a43b6d2812d9353
Jiri Drbalek <jiri.drbalek@firma.seznam.cz>
2019-09-18 22:04:18 +0200

    w

---
+++ icu4c/source/test/cintltst/scrptruntst.c
@@ -0,0 +1,19 @@
+// © 2016 and later: Unicode, Inc. and others.
+// License & terms of use: http://www.unicode.org/copyright.html
+/********************************************************************
+ * COPYRIGHT: 
+ * Copyright (c) 1998-2016, International Business Machines Corporation and
+ * others. All Rights Reserved.
+ ********************************************************************/
+/*
+* File putiltst.c (Tests the API in putil)
+*
+* Modification History:
+*
+*   Date          Name        Description
+*   07/12/2000    Madhu       Creation 
+*******************************************************************************
+*/
+
+#include "scrptrun.h"
+
